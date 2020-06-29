3834977161fe1175767f8d0060f8a641dc932b50
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2020-06-29 12:34:56 -0400

    basic functions update

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ const URL = '/api/ron/quotes';
 
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('ron').addEventListener('click', getRon);
-  document.getElementById('yep').addEventListener('click', getYep);
 });
 
 function getRon(ev) {
@@ -15,16 +14,4 @@ function getRon(ev) {
       main.innerHTML = `<h2>${content[0]}</h2>`;
     })
     .catch((err) => console.error);
-}
-
-function getYep(ev) {
-  ev.preventDefault();
-  console.log('yep');
-  fetch('/.netlify/functions/yep')
-    .then((res) => res.json())
-    .then((data) => {
-      let main = document.querySelector('main');
-      main.innerHTML = `<h2>${data}</h2>`;
-    })
-    .catch((err) => console.error(err));
 }
