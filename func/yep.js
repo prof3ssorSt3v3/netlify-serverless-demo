189ea783c571bc1842eb1f6db1fc523015769cdd
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2020-06-29 12:25:24 -0400

    use yep callback

--- func/yep.js
+++ func/yep.js
@@ -1,13 +1,13 @@
 // /.netlify/functions/yep
 
-exports.handler = (event, context) => {
+exports.handler = (event, context, callback) => {
   //event = Express Request object (similar)
   console.log(event);
   let steve = process.env.STEVE;
   console.log(steve);
 
-  return {
+  callback(null, {
     statusCode: 200,
     body: JSON.stringify(`Yep. yep. yep. ${steve}`),
-  };
+  });
 };
