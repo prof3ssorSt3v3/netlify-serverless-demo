50ab7325ddfda9d40c671d7bd954b3a65d23e394
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2020-06-29 12:50:12 -0400

    added ENV variable STEVE

--- func/yep.js
+++ func/yep.js
@@ -4,8 +4,11 @@ exports.handler = function (event, context, callback) {
   //event - similar to express Request object
   console.log(event);
 
+  let steve = process.env.STEVE;
+  console.log(steve);
+
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({ msg: 'Yep. Yep. Yep.' }),
+    body: JSON.stringify({ msg: `Yep. Yep. Yep. ${steve}` }),
   });
 };
